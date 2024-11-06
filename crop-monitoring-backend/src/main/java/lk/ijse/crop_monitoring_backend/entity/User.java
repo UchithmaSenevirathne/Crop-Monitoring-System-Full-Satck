9df8bf026a741271f51636c1d3f09bd7b7ba2c8d
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-06 13:41:22 +0530

    create user entity class

---
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/entity/User.java
@@ -0,0 +1,21 @@
+package lk.ijse.crop_monitoring_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private int userId;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+}
