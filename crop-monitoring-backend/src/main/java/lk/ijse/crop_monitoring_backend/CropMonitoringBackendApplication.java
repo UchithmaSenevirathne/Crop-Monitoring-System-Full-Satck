0ba56f7db337f4708b4a27cdb48ce55ea3dbd529
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-06 12:35:45 +0530

    Initial commit of fullstack

---
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/CropMonitoringBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropMonitoringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropMonitoringBackendApplication.class, args);
+	}
+
+}
---
+++ crop-monitoring-backend/src/test/java/lk/ijse/crop_monitoring_backend/CropMonitoringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.crop_monitoring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropMonitoringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
