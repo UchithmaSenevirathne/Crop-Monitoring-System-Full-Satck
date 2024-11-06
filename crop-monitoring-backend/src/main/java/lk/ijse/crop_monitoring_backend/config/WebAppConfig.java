561866265620bee25e265c0354a28a330a97a1a4
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-06 13:04:12 +0530

    create webappconfig file

---
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/config/WebAppConfig.java
@@ -0,0 +1,19 @@
+package lk.ijse.crop_monitoring_backend.config;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.crop_monitoring_backend")
+@EnableTransactionManagement
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 2,  //2MB
+        maxFileSize = 1024 * 1024 * 10, //10MB
+        maxRequestSize = 1024 * 1024 * 50 //50MB
+)
+public class WebAppConfig {
+}
