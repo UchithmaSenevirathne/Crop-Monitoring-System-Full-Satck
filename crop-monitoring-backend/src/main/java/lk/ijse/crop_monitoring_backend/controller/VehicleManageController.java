1cdae5b2763eb89cb525168fe5b056b20bd21dbc
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-11 21:29:25 +0530

    get vehicle categories, fuel types and status from enums

--- crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/VehicleManageController.java
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/VehicleManageController.java
@@ -5,12 +5,20 @@ import lk.ijse.crop_monitoring_backend.dto.VehicleDTO;
 import lk.ijse.crop_monitoring_backend.exception.DataPersistFailedException;
 import lk.ijse.crop_monitoring_backend.service.EquipmentService;
 import lk.ijse.crop_monitoring_backend.service.VehicleService;
+import lk.ijse.crop_monitoring_backend.util.Enums.Availability;
+import lk.ijse.crop_monitoring_backend.util.Enums.FieldName;
+import lk.ijse.crop_monitoring_backend.util.Enums.FuelType;
+import lk.ijse.crop_monitoring_backend.util.Enums.VehicleCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/vehicle")
@@ -31,5 +39,29 @@ public class VehicleManageController {
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(value = "/categories", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getVehicleCategories() {
+        List<String> categories = Arrays.stream(VehicleCategory.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping(value = "/fuel", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getFuelTypes() {
+        List<String> fuel = Arrays.stream(FuelType.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(fuel);
+    }
+
+    @GetMapping(value = "/status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getStatus() {
+        List<String> status = Arrays.stream(Availability.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(status);
     }
 }
