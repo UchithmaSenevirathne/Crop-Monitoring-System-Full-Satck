f220228171dcdaf89c6d72a3e9e7c4d40df2f9af
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-11 21:22:10 +0530

    get crop categories and seasons from enums

--- crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/CropManageController.java
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/CropManageController.java
@@ -5,6 +5,9 @@ import lk.ijse.crop_monitoring_backend.exception.DataPersistFailedException;
 import lk.ijse.crop_monitoring_backend.service.CropService;
 import lk.ijse.crop_monitoring_backend.service.FieldService;
 import lk.ijse.crop_monitoring_backend.util.AppUtil;
+import lk.ijse.crop_monitoring_backend.util.Enums.CropCategory;
+import lk.ijse.crop_monitoring_backend.util.Enums.CropSeason;
+import lk.ijse.crop_monitoring_backend.util.Enums.FieldName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/crop")
@@ -52,5 +59,21 @@ public class CropManageController {
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(value = "/category", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getCropCategories() {
+        List<String> cropCategories = Arrays.stream(CropCategory.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(cropCategories);
+    }
+
+    @GetMapping(value = "/season", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getCropSeasons() {
+        List<String> cropSeasons = Arrays.stream(CropSeason.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(cropSeasons);
     }
 }
