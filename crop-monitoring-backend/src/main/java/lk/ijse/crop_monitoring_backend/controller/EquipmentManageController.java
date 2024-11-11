4b8af1c4c85eaf9146bb649c8e15f37d1b3b9f05
UchithmaSenevirathne <uchithma@gmail.com>
2024-11-11 21:47:14 +0530

    get equipment names, types and status from enums

--- crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/EquipmentManageController.java
+++ crop-monitoring-backend/src/main/java/lk/ijse/crop_monitoring_backend/controller/EquipmentManageController.java
@@ -5,12 +5,18 @@ import lk.ijse.crop_monitoring_backend.dto.StaffDTO;
 import lk.ijse.crop_monitoring_backend.exception.DataPersistFailedException;
 import lk.ijse.crop_monitoring_backend.service.EquipmentService;
 import lk.ijse.crop_monitoring_backend.service.StaffService;
+import lk.ijse.crop_monitoring_backend.util.Enums.Availability;
+import lk.ijse.crop_monitoring_backend.util.Enums.EquipmentName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/equipment")
@@ -41,5 +47,29 @@ public class EquipmentManageController {
         }catch (Exception e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    @GetMapping(value = "/names", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getEquipmentNames() {
+        List<String> names = Arrays.stream(EquipmentName.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(names);
+    }
+
+    @GetMapping(value = "/types", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getEquipmentTypes() {
+        List<String> types = Arrays.stream(Availability.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(types);
+    }
+
+    @GetMapping(value = "/status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<String>> getStatus() {
+        List<String> status = Arrays.stream(Availability.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(status);
     }
 }
