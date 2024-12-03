0519cd8ca57d6f13cb281912415bb0924ec32214
UchithmaSenevirathne <uchithma@gmail.com>
2024-12-04 04:54:47 +0530

    image input changed

---
+++ crop-monitoring-frontend/js/field.js
@@ -0,0 +1,23 @@
+function previewImageField1(event) {
+    const file = event.target.files[0]; // Get the selected file
+    if (file) {
+      const reader = new FileReader();
+    //   reader.onload = function (e) {
+    //     const preview = document.getElementById('image-preview-field1'); // Target the preview image element
+    //     preview.src = e.target.result; // Set the image source
+    //   };
+      reader.readAsDataURL(file); // Read the file as a Data URL
+    }
+  }
+
+  function previewImageField2(event) {
+    const file = event.target.files[0]; // Get the selected file
+    if (file) {
+      const reader = new FileReader();
+    //   reader.onload = function (e) {
+    //     const preview = document.getElementById('image-preview-field1'); // Target the preview image element
+    //     preview.src = e.target.result; // Set the image source
+    //   };
+      reader.readAsDataURL(file); // Read the file as a Data URL
+    }
+  }
